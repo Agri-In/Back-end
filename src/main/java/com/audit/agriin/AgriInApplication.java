cace982576435ec718194e48622bec46a659b5c5
Sidati nouhi <sidatnouhi@gmail.com>
2024-03-05 09:34:11 +0100

    The project structure

---
+++ src/main/java/com/audit/agriin/AgriInApplication.java
@@ -0,0 +1,13 @@
+package com.audit.agriin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/audit/agriin/AgriInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audit.agriin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
